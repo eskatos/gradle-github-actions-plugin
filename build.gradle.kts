2b1c0db35a7e6e61efcfb260b987c8e15cc6b486
Paul Merlin <paul@nosphere.org>
2020-06-06 11:14:29 +0200

    Prepare next development iteration

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "org.nosphere.gradle.github"
-version = "1.2.0"
+version = "1.3.0-SNAPSHOT"
 
 pluginBundle {
     website = "https://github.com/eskatos/gradle-github-actions-plugin"
