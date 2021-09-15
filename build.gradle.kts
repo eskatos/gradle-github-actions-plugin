2ede652bcad3511554c186e25346248b834ddadd
Paul Merlin <paul@nosphere.org>
2021-09-15 18:21:49 +0200

    Prepare 1.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "org.nosphere.gradle.github"
-version = "1.3.0-SNAPSHOT"
+version = "1.3.0"
 
 pluginBundle {
     website = "https://github.com/eskatos/gradle-github-actions-plugin"
