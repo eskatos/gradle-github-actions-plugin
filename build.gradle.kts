e70a4da9a0476a1ac3220c313cab15fcca806d0a
Paul Merlin <paul@nosphere.org>
2021-09-15 18:24:53 +0200

    Dogfood v1.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     `kotlin-dsl`
     `maven-publish`
     id("com.gradle.plugin-publish") version "0.15.0"
-    id("org.nosphere.gradle.github.actions") version "1.2.0"
+    id("org.nosphere.gradle.github.actions") version "1.3.0"
 }
 
 group = "org.nosphere.gradle.github"
