82a9fc79b523ea4d78e8b802bf438b786fe36cfb
Paul Merlin <paul@nosphere.org>
2023-10-17 13:34:27 +0200

    Update plugin publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,26 @@
 plugins {
     `kotlin-dsl`
     `maven-publish`
-    id("com.gradle.plugin-publish") version "0.16.0"
+    id("com.gradle.plugin-publish") version "1.2.1"
     id("org.nosphere.gradle.github.actions") version "1.3.2"
 }
 
 group = "org.nosphere.gradle.github"
 version = "1.4.0-SNAPSHOT"
 
-pluginBundle {
-    website = "https://github.com/eskatos/gradle-github-actions-plugin"
-    vcsUrl = "https://github.com/eskatos/gradle-github-actions-plugin"
-    description = "Gradle Plugin for Github Actions"
-    tags = listOf("github-actions", "scans")
+gradlePlugin {
     plugins {
         named("org.nosphere.gradle.github.actions") {
             displayName = "Gradle Github Actions Plugin"
+            description = "Gradle Plugin for Github Actions"
         }
     }
+}
+
+pluginBundle {
+    website = "https://github.com/eskatos/gradle-github-actions-plugin"
+    vcsUrl = "https://github.com/eskatos/gradle-github-actions-plugin"
+    tags = listOf("github-actions", "scans")
 }
 
 java {
